06e2d1742844368f6089600cc81774acac620d66
Imam Alfarizi <imam.alfarizi.777@gmail.com>
2024-01-07 17:55:45 +0700

    first commit

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,18 @@
+import { Collaborate } from './_components/collaborate';
+import { GetInTouch } from './_components/get-in-touch';
+import { Hero } from './_components/hero';
+import { Section1 } from './_components/section-1';
+
+export default function ContactUs() {
+  return (
+    <>
+      <Hero />
+
+      <Section1 />
+
+      <Collaborate />
+
+      <GetInTouch />
+    </>
+  );
+}
